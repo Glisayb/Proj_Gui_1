c2089f111137ca89135d1b71b4a73c4303d1dfcb
karpiesiuk.marcin <karpiesiuk.marcin@gmail.com>
2022-04-21 10:42:43 +0200

    zmiany

---
+++ src/Persistance/PersistanceStatics.java
@@ -0,0 +1,60 @@
+package Persistance;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PersistanceStatics {
+
+    public static class PatternProperties {
+
+        public static String getStringProperty(String input, Pattern pattern){
+            Matcher propertyMatcher = pattern.matcher(input);
+            propertyMatcher.find();
+            return propertyMatcher.group(1);
+        }
+
+        public static  int getIntProperty(String input, Pattern pattern){
+            Matcher propertyMatcher = pattern.matcher(input);
+            propertyMatcher.find();
+            return Integer.parseInt(propertyMatcher.group(1));
+        }
+
+        public static  double getDoubleProperty(String input, Pattern pattern){
+            Matcher propertyMatcher = pattern.matcher(input);
+            propertyMatcher.find();
+            return Double.parseDouble(propertyMatcher.group(1));
+        }
+    }
+
+    public static class FilePersistance{
+
+        public static void WriteFile(String stringPath, String content){
+            try {
+                var path = Paths.get(stringPath);
+                if(!Files.exists(path)){
+                    Files.createFile(path);
+                }
+                Files.write(path, content.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static String Read(String stringPath){
+            try {
+                var path = Paths.get(stringPath);
+                if(Files.exists(path)){
+                    var content = Files.readAllLines(path);
+                    return String.join("\n", content);
+                }
+                else
+                    throw new FileNotFoundException();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
